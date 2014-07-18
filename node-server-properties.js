0348552f4ecd4c86180655bdb4be93870dbd52d7
Bruna Xavier <brunabxsantos@gmail.com>
2014-07-18 15:06:01 -0300

    Propriedades do servidor

--- node-server-properties.js
+++ node-server-properties.js
@@ -1,13 +1,13 @@
-// Properties
-var Properties = {};
+﻿// Properties
+var Properties = process.env || {};
 
 // constants
-Properties.SPREADSHEET_NAME = '';
-Properties.SECTIONS_WORKSHEET_NAME = '';
-Properties.TASKS_WORKSHEET_NAME = '';
-Properties.TAKSLOGS_WORKSHEET_NAME = '';
-Properties.USERNAME = '';
-Properties.PASSWORD = '';
+//Properties.SPREADSHEET_NAME = '';
+//Properties.SECTIONS_WORKSHEET_NAME = '';
+//Properties.TASKS_WORKSHEET_NAME = '';
+//Properties.TAKSLOGS_WORKSHEET_NAME = '';
+//Properties.USERNAME = '';
+//Properties.PASSWORD = '';
 
 Properties.LOCAL_DATA_REFRESH = 60000;
 Properties.GOOGLE_DRIVE_LOG = false;
