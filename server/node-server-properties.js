1575620ae48c533e9c758f3cb851c02aa487bc72
Bruna Xavier <brunabxsantos@gmail.com>
2014-07-17 21:50:50 -0300

    node-server-properties.js

---
+++ server/node-server-properties.js
@@ -0,0 +1,19 @@
+// Properties
+var Properties = {};
+
+// constants
+Properties.SPREADSHEET_NAME = '';
+Properties.SECTIONS_WORKSHEET_NAME = '';
+Properties.TASKS_WORKSHEET_NAME = '';
+Properties.TAKSLOGS_WORKSHEET_NAME = '';
+Properties.USERNAME = '';
+Properties.PASSWORD = '';
+
+Properties.LOCAL_DATA_REFRESH = 60000;
+Properties.GOOGLE_DRIVE_LOG = false;
+Properties.SERVER_APP_LOG = false;
+
+// constants
+
+// export
+module.exports = Properties;
